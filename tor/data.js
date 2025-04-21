7a50e052941e412e0266cac76eb7b0b473b4879f
Willy <lmaovuu@gmail.com>
2025-04-20 23:39:58 -0400

    Update marketplace data - 2025-04-21T03:40:00.288Z

--- tor/data.js
+++ tor/data.js
@@ -1,1 +1,6 @@
-
+const marketplaceData = {
+  "lastUpdated": "2025-04-21T03:40:00.040Z",
+  "items": [],
+  "status": "error",
+  "errorMessage": "Tor is not running or not configured correctly"
+};
